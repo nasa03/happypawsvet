ded90ae89ead1a270967bb5cceb3de8bb2582b5c
loverde13 <jakob@loud-20.04>
2021-09-06 22:05:56 -0400

    Added spinners and card. Carousel and scrollspy are WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CarouselComponent } from './carousel/carousel.component';
 import { BotNavBarComponent } from './Footer/bot-nav-bar.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { ScrollSpyComponent } from './scrollspy/scrollspy.component';
 
 @NgModule({
-  declarations: [AppComponent,TopNavBarComponent, BotNavBarComponent],
+  declarations: [AppComponent,TopNavBarComponent, 
+    BotNavBarComponent, CarouselComponent,
+  CardComponent, ScrollSpyComponent],
   imports: [BrowserModule, FormsModule],
   providers: [AppComponent],
   bootstrap: [AppComponent]
---
+++ src/app/scrollspy/scrollspy.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-scrollspy',
+    templateUrl: 'scrollspy.component.html'
+})
+export class ScrollSpyComponent{
+}
